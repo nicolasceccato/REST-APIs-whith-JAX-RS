4774da21855a340a7d01f5d345b54123ae32ce9e
nicolasceccato <nicolasceccato@hotmail.com>
2023-02-18 14:31:27 -0300

    Criada a classe ProfileService

---
+++ Project/messenger/src/main/java/org/koushik/javabrains/messenger/service/ProfileService.java
@@ -0,0 +1,45 @@
+package org.koushik.javabrains.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.koushik.javabrains.messenger.database.DatabaseClass;
+import org.koushik.javabrains.messenger.model.Profile;
+
+public class ProfileService {
+
+	Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	ProfileService(){
+		profiles.put("João", new Profile(1L, "João", "Joãonelso","Nelso" ));
+		profiles.put("Pedro", new Profile(2L, "Pedro", "Pedronson", "Nelso"));
+	}
+
+	public List<Profile> getAllProfiles() {
+		return new ArrayList<Profile>(profiles.values());
+	}
+
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+	}
+
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile updateProfile(Profile profile) {
+		if (profile.getProfileName().isEmpty()) {
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile removeProfile(String profileName) {
+		return profiles.remove(profileName);
+	}
+
+}
